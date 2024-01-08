9f70f1a64b543fcdbf87721713ff42a605272ab1
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-01-08 11:32:14 +0100

    create macro-sections

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>prova</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeaderVue',
+    data(){
+        return {
+        }
+    },
+}
+
+
+</script>
+<style lang="scss" scoped>
+    
+</style>
